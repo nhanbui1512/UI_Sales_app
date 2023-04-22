28a8dd497cb303fb33e79581f29ebcc7ebd005af
= <=buithiennhan0345@gmail.com>
2023-04-22 10:08:52 +0700

    Update hande API

--- app/src/main/java/com/example/tikoshopping/API/APIPostSales.java
+++ app/src/main/java/com/example/tikoshopping/API/APIPostSales.java
@@ -1,15 +1,28 @@
 package com.example.tikoshopping.API;
 
+import com.example.tikoshopping.Service.ResultBase;
 import com.example.tikoshopping.Service.ResultPostSales;
 import com.example.tikoshopping.Service.ResultTypeGoods;
+import com.example.tikoshopping.Service.Sales.FormCreatePostSales;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.List;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Part;
 import retrofit2.http.Query;
 import retrofit2.http.QueryMap;
 
@@ -34,5 +47,35 @@ public interface APIPostSales {
     })
     @GET("/api/sales/getrand")
     Call<ResultPostSales> getPostSalesRandom(@Query("rand_number") int randNumber);
+
+
+    // Xóa bài bán hàng
+    @Headers({
+            "Content-Type: application/json",
+            "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRFVzZXIiOjEsImFjY2VzcyI6MCwidXNlck5hbWUiOiJhZG1pbiIsImlhdCI6MTY4MTA0OTQxOSwiZXhwIjoxNjgzNjQxNDE5fQ.vHEF_WIe1D_g3IzdkjHRL4vGasFaNB3rlATBRZw89uE"
+    })
+    @DELETE("/api/sales/delete")
+    Call<ResultBase> DeletePostSales (@Query("id_post") int idPost);
+
+    // Tạo 1 bài bán hàng
+
+    @Headers({
+            "Content-Type: application/json",
+            "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRFVzZXIiOjEsImFjY2VzcyI6MCwidXNlck5hbWUiOiJhZG1pbiIsImlhdCI6MTY4MTA0OTQxOSwiZXhwIjoxNjgzNjQxNDE5fQ.vHEF_WIe1D_g3IzdkjHRL4vGasFaNB3rlATBRZw89uE"
+    })
+    @POST("/api/sales/add")
+    Call<ResultBase> createPostSales(@Part("title") RequestBody title,
+                                     @Part("description") RequestBody description,
+                                     @Part("idType") RequestBody idType,
+                                     @Part List<MultipartBody.Part> photos);
+
+
+    @Headers({
+            "Content-Type: application/json",
+            "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRFVzZXIiOjEsImFjY2VzcyI6MCwidXNlck5hbWUiOiJhZG1pbiIsImlhdCI6MTY4MTA0OTQxOSwiZXhwIjoxNjgzNjQxNDE5fQ.vHEF_WIe1D_g3IzdkjHRL4vGasFaNB3rlATBRZw89uE"
+    })
+    @GET("/api/sales/getbytypeid")
+    Call<ResultPostSales> getPostSalesByTypeGoodsID(@Query("id_type") int idType);
+
 
 }
--- app/src/main/java/com/example/tikoshopping/API/APITypeGoods.java
+++ app/src/main/java/com/example/tikoshopping/API/APITypeGoods.java
@@ -2,18 +2,23 @@ package com.example.tikoshopping.API;
 
 import com.example.tikoshopping.Service.Login;
 import com.example.tikoshopping.Service.LoginResult;
+import com.example.tikoshopping.Service.ResultBase;
 import com.example.tikoshopping.Service.ResultTypeGoods;
 import com.example.tikoshopping.Service.UserData;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface APITypeGoods {
     Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
@@ -28,5 +33,19 @@ public interface APITypeGoods {
     })
     @GET("/api/type/getall")
     Call<ResultTypeGoods> getAllType();
+
+
+
+    @Headers({
+            "Content-Type: application/json",
+            "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRFVzZXIiOjEsImFjY2VzcyI6MCwidXNlck5hbWUiOiJhZG1pbiIsImlhdCI6MTY4MTA0OTQxOSwiZXhwIjoxNjgzNjQxNDE5fQ.vHEF_WIe1D_g3IzdkjHRL4vGasFaNB3rlATBRZw89uE"
+    })
+    @Multipart
+    @POST("/api/type/add")
+    Call<ResultBase> createTypeGoods(
+            @Part("nameType") RequestBody nameType,
+            @Part MultipartBody.Part icon
+    );
+
 
 }
